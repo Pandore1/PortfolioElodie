2fb7ecdd5626eff6f81744391f0b00bff8f5a390
Pandore01 <elodie491@hotmail.com>
2024-10-10 14:57:23 -0400

    Burger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,17 @@
 import { useCallback, useState } from "react"
 
 export default function Navbar(){
+    const [burgerOpen, setBurgerOpen] = useState(false);
+    const toggleBurgerNav=()=>{
+    setBurgerOpen(!burgerOpen);
+}
     return(
-        <nav className="menuBox">
-            <a  href="#"><h1 className="logoName">Logo</h1></a>
+        <nav  className={`menuBox${burgerOpen?' open':''}`}>
+            <button className="burgerIcon" onClick={toggleBurgerNav}>
+                <span className="burgerLine"></span>
+                <span className="burgerLine"></span>
+                <span className="burgerLine"></span>
+            </button>
             <ul className="menu col-5">
                 <li className="tab"><a className="tabLink" link-name='Accueil' href="">Accueil</a></li>
                 <li className="tab"><a className="tabLink" link-name='Réalisation' href="">Réalisation</a></li>
